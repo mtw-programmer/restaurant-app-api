ce9a875462084d7b2a478a71dfbda228fc7e4e26
mtw-programmer <visnia58@gmail.com>
2023-08-09 12:22:38 +0200

    feat: create exit util to exit app and log msg on error

---
+++ utils/exit.ts
@@ -0,0 +1,8 @@
+import log from './log';
+
+interface LogError { label: string; message: string };
+
+export default ({ label, message }:LogError):void => {
+  log.error({ label, message });
+  process.exit(1);
+};
