d36cbfda0b54cfe0651eee1ad0fc75dc20c3a646
mtw-programmer <visnia58@gmail.com>
2023-08-09 12:36:41 +0200

    fix: update config import and replace process.env

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import log from './utils/log';
-import './utils/config';
+import config from './utils/config';
 
 const app = express();
 
 app.use(express.json());
 
-const port = process.env.PORT || 3001;
+const port = config.APP_PORT || 3001;
 app.listen(port, () => log.init({ label: 'Index', message: `App listening on port ${port}` }));
