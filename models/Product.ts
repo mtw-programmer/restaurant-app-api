2eafeb79ef3c83de282480a2306132af10d35f8c
mtw-programmer <visnia58@gmail.com>
2023-08-11 12:53:42 +0200

    feat: create Product model

---
+++ models/Product.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    minLength: 1,
+    maxLength: 30,
+    required: true 
+  },
+  img: {
+    type: String,
+    minLength: 1,
+    maxLength: 128,
+    required: true 
+  },
+  description: {
+    type: String,
+    minLength: 1,
+    maxLength: 1024,
+    required: true 
+  },
+  price: {
+    type: Number,
+    min: 0.01,
+    max: 9999999,
+    required: true
+  }
+});
+
+export default mongoose.model('Product', schema);
