d136b3161d3a7dfde8365984b481fe274023a070
mtw-programmer <visnia58@gmail.com>
2023-08-14 00:47:09 +0200

    feat: add verifyToken route with authorization middleware

--- routes/index.ts
+++ routes/index.ts
@@ -3,11 +3,14 @@ import { Router } from 'express';
 import getProducts from './getProducts';
 import getOffers from './getOffers';
 import authentication from './authentication';
+import authorization from '../middleware/authorization';
+import verifyToken from './verifyToken';
 
 const router = Router();
 
 router.use('/get-products', getProducts);
 router.use('/get-special-offers', getOffers);
 router.use('/authentication', authentication);
+router.use('/verify-token', [authorization, verifyToken]);
 
 export default router;
