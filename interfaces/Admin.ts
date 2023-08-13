e96b9ad3d2b752c7602f4271882108f322e08ed5
mtw-programmer <visnia58@gmail.com>
2023-08-13 20:25:02 +0200

    fix: add _id to Admin interface

--- interfaces/Admin.ts
+++ interfaces/Admin.ts
@@ -1,6 +1,7 @@
-import { Document } from 'mongoose';
+import { ObjectId, Document } from 'mongoose';
 
 export default interface Admin extends Document {
+  _id: ObjectId;
   username: string;
   email: string;
   password: string;
