5fe1440f956d1af954405b02d696fe1f259c0095
mtw-programmer <visnia58@gmail.com>
2023-08-13 14:56:56 +0200

    feat: update clearDB helper for tests

--- tests/integration/helpers/clearDB.ts
+++ tests/integration/helpers/clearDB.ts
@@ -1,5 +1,7 @@
 import Product from '../../../models/Product';
+import Offer from '../../../models/Offer';
 
 export default async () => {
   await Product.deleteMany({});
+  await Offer.deleteMany({});
 };
