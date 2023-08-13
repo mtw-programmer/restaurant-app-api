0a399e9c645bd6624edea6e3ca7f93b712944bc9
mtw-programmer <visnia58@gmail.com>
2023-08-13 20:38:48 +0200

    fix: make router error handler message universal

--- utils/errorHandling/router.ts
+++ utils/errorHandling/router.ts
@@ -1,7 +1,7 @@
 import { Response } from 'express';
 import log from '../log';
 
-export default (res: Response, msg: string):Response => {
-  log.error({ label: 'getProducts', message: `${msg}` });
+export default (label: string, res: Response, msg: string):Response => {
+  log.error({ label, message: `${msg}` });
   return res.status(500).json({ msg: 'Something went wrong! Please, try again later.' });
 };
