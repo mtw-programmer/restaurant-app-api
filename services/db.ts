8554cb851a65b46dd6376f843fa07fd818be5433
mtw-programmer <visnia58@gmail.com>
2023-09-03 11:07:53 +0200

    feat: add mongoose on event listeners for db service
    add log and process.exit if needed for open, disconnected, reconnected, connected and SIGNIT listeners

--- services/db.ts
+++ services/db.ts
@@ -3,5 +3,29 @@ import config from '../utils/config';
 import log from '../utils/log';
 
 mongoose.connect(`mongodb://${config.DB_HOST}:${config.DB_PORT}/${config.DB_NAME}`)
-  .then(() => log.init({ label: 'DB', message: `Connected to ${config.DB_NAME} database on port ${config.DB_PORT}` }))
-  .catch((ex) => log.error({ label: 'DB', message: ex }));
+  .catch((ex) => {
+    log.error({ label: 'DB', message: ex });
+    process.exit(1);
+  });
+
+mongoose.connection.on('open', () =>
+  log.info({ label: 'DB', message: `Opened connection to ${config.DB_NAME} database on port ${config.DB_PORT}` })
+);
+
+mongoose.connection.on('disconnected', () => {
+  log.error({ label: 'DB', message:  'Lost connection to MongoDB database!' });
+});
+
+mongoose.connection.on('reconnected', () => {
+  log.info({ label: 'DB', message:  'Reconnected to MongoDB database' });
+});
+
+mongoose.connection.on('connected', () =>
+  log.info({ label: 'DB', message: 'Established MongoDB connection' })
+);
+
+process.on('SIGINT', () => {
+  log.error({ label: 'DB', message: 'Forced to close MongoDB connection!' });
+  mongoose.connection.close();
+  process.exit(1);
+});
